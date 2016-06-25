364c9bef074717033657e1bc1504ffc9261c9e1c
jordanSu <newjordansu1126@gmail.com>
2016-06-25 14:11:11 +0800

    add static library

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
-gcc -DGLEW_STATIC -I. -c glew.c
-g++ -o HW4 -static -O2 -std=c++0x -DGLEW_STATIC main.cpp tiny_obj_loader.cc glew.o -I.  -lglfw3 -lopengl32 -lgdi32
-strip HW4.exe
+gcc -DGLEW_STATIC -g3 -I. -c glew.c
+g++ -c -std=c++0x -g3 -DGLEW_STATIC Mesh.cpp
+g++ -c -std=c++0x -g3 -DGLEW_STATIC Model.cpp
+g++ -o HW4 -static -O2 -std=c++0x -g3 -DGLEW_STATIC main.cpp tiny_obj_loader.cc glew.o Mesh.o Model.o -L./lib/ -lSOIL -I. -lglfw3 -lopengl32 -lgdi32 -lassimp
